ee876a22db485335e8fda1340211b68bc4d25fe2
Siddhartha Vibhu Pharswan <{ID}+{username}@users.noreply.github.com>
2024-02-05 00:30:45 +0530

    pg client integration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const { createClient } = require('@vercel/postgres');
+const { Client } = require('pg');
 
+const axios = require('axios');
 const bodyparser = require('body-parser');
 const cors = require('cors');
 const app = express()
@@ -11,21 +12,17 @@ app.use(cors({
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }))
 
-const client = createClient({
-    // connectionString: 'postgres://default:{password}@{host}/{database}',
-    connectionString: 'postgres://default:lVaf7WMOKJ8Y@ep-soft-bread-a44ca8gy.us-east-1.postgres.vercel-storage.com:5432/verceldb'
-})
-
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-
+const client = new Client({ connectionString: 'postgres://default:lVaf7WMOKJ8Y@ep-soft-bread-a44ca8gy.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require' });
 app.get('/add-url', async (req, res) => {
     const { email, url } = req.query;
     try {
-        await client.sql`CREATE TABLE IF NOT EXISTS url_tracker (Url varchar(255), email varchar(255));`;
-        const data = await client.sql`INSERT INTO url_tracker (Url, email) VALUES (${url}, ${email});`;
+        const d = await client.query(`CREATE TABLE IF NOT EXISTS url_tracker (url varchar(255), email varchar(255));`);
+        console.log(d);
+        const data = await client.query(`INSERT INTO url_tracker (Url, email) VALUES (${url}, ${email});`);
         console.log(data);
-        res.send(data);
+        res.send(data || []);
     } catch (err) {
         console.error(err);
         res.send(err);
@@ -34,20 +31,21 @@ app.get('/add-url', async (req, res) => {
 
 app.get('/history', async (req, res) => {
     const { search, email } = req.query;
-    await client.sql`CREATE TABLE IF NOT EXISTS url_tracker (Url varchar(255), User_id varchar(255));`;
+    const data = await client.query(`CREATE TABLE IF NOT EXISTS url_tracker (url varchar(255), email varchar(255));`);
+    console.log(data);
     axios.get(`https://www.googleapis.com/oauth2/v2/userinfo?access_token=${email}`)
         .then(async (response) => {
             if (!search) {
-                const { rows } = await client.sql`SELECT * from url_tracker WHERE email = ${response?.data?.email};`;
+                const { rows } = await client.query(`SELECT * from url_tracker WHERE email='${response?.data?.email}';`);
                 console.log(rows);
-                return res.json(rows);
+                return res.json(rows || []);
             } else {
-                const { rows } = await client.sql`SELECT * from url_tracker WHERE email=${response?.data?.email} AND Url LIKE ${'%' + search + '%'};`;
+                const { rows } = await client.query(`SELECT * from url_tracker WHERE email='${response?.data?.email}' AND Url LIKE '${'%' + search + '%'}';`);
                 console.log(rows);
-                return res.json(rows);
+                return res.json(rows || []);
             }
         }).catch((error) => {
-            setError(error?.message)
+            console.log(error);
         })
 })
 
@@ -60,5 +58,6 @@ client.connect().then(() => {
         console.log(`Example app listening on port ${port}`)
     })
 })
+
 
 module.exports = app;
